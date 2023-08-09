69c94e5f585523207296f0a71183614a758316aa
root <root@NCEL144582.iis.amadeus.net>
2023-08-09 16:17:33 +0200

    update

--- main.go
+++ main.go
@@ -5,64 +5,73 @@ import (
 	"strconv"
 )
 
-func strcustom(s string) []string{
+func strcustom(s string) []string {
 	// convert string to Int
-	n,_ := strconv.Atoi(s)
-	fmt.Println(n)
+	n, _ := strconv.Atoi(s)
 	var res []string
-	if n < 10{
-		res = append(res, s) 
+	if n < 10 {
+		res = append(res, s)
 	} else {
-		for _,i:= range(s){
+		for _, i := range s {
 			// convert unicode to string
-			fmt.Println(string(i))
+			// fmt.Println(string(i))
 			res = append(res, string(i))
 		}
 	}
 	return res
 }
-func compress(chars []byte) []string {
-	var current byte = chars[0]
+func compress(chars []byte) int {
+	_chars := &chars
+	var current byte = (*_chars)[0]
 	var count int = 1
 	res := []string{}
-	for i:=1; i< len(chars); i++ {
-		fmt.Println(string(chars[i]))
-		if chars[i] == current {
+	for i := 1; i < len(*_chars); i++ {
+		if (*_chars)[i] == current {
 			count++
-			fmt.Println("YES - ",string(current), count)
-		} else{ 
-			if count == 1{
-				fmt.Println("CORRECT")
-				res = append (res, string(current))
-			}else{
-				res = append (res, string(current))
-				fmt.Println("current: ", string(current), "count: ", strconv.Itoa(count))
-				res = append (res, strconv.Itoa(count))
+			fmt.Println(count)
+		} else {
+			if count == 1 {
+				res = append(res, string(current))
+			} else {
+				res = append(res, string(current))
+				// add 2 slices
+				res = append(res, strcustom(strconv.Itoa(count))...)
 			}
-			current = chars[i+1]
+			current = (*_chars)[i+1]
 			count = 1
 		}
 	}
-	if count == 1{
+	if count == 1 {
 		res = append(res, string(current))
-	}else{
+	} else {
 		res = append(res, string(current))
-		res = append(res, strconv.Itoa(count))
+		res = append(res, strcustom(strconv.Itoa(count))...)
 	}
-	return res
+	//chars = res
+	//resB:=[]byte{}
+	// st_ := ""
+	// for _, i := range res {
+	// 	st_ = st_ + i
+	// }
+	var s string = ""
+	for _, i := range res {
+		s = s + i
+	}
+	// var b = chars
+	a := []byte(s)
+
+	for idx, _ := range a {
+		(*_chars)[idx] = a[idx]
+	}
+
+	return len(res)
+	//return chars
 }
 
 func main() {
-	
-	// chars := []byte{'a','b','b','b','b','b','b','b','b','b','b','b','b'}
-	//fmt.Println(compress(chars))
-	fmt.Println(strcustom("10"))
-	// s,err:=strconv.Atoi("10")
-	// if err!=nil{
-	// 	fmt.Println("wrong input")
-	// }else{
-	// 	fmt.Println(s+1)
-	// }
-	
 
+	chars := []byte{'a', 'b', 'c'}
+	fmt.Println(chars)
+	fmt.Println(compress(chars))
+	fmt.Println(chars)
 }
